dfdc4e306e76db6eef06a513ad8e634aa672e9fa
TomozQ <inf.4clover@gmail.com>
2020-09-03 22:31:30 +0900

    openAnimation

---
+++ app/assets/javascripts/TopOfTop.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.top__wrapper__backs__back1').slideUp(1800);
+  let count = 0;
+  let openSite = function(){
+    $('.top__wrapper__backs__back2').slideUp(1500);
+  }
+  setTimeout(openSite, 10000)
+});
+
+
+
+
