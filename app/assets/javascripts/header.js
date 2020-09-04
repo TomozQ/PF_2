9a3266ff140c13da3241b167e0b51d2929b04b96
TomozQ <inf.4clover@gmail.com>
2020-09-04 18:53:09 +0900

    linkのjs変更

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,8 +1,15 @@
 $(function(){
   $('button').hover(function(){
-    $('span').css('display', 'inline-block')
+    $('span').css('display', 'inline-block');
   },
   function(){
-    $('span').css('display', 'none')
+    $('span').css('display', 'none');
+  });
+
+  $('a').hover(function(){
+    $(this).css('font-weight', 'bold');
+  },
+  function(){
+    $(this).css('font-weight', 'lighter');
   });
 });
