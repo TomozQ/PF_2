fdc66913fe52abbf9a467cf8427fce3a74c4cf0c
TomozQ <inf.4clover@gmail.com>
2020-09-04 10:23:32 +0900

    slideUpTime調整

--- app/assets/javascripts/TopOfTop.js
+++ app/assets/javascripts/TopOfTop.js
@@ -1,9 +1,9 @@
 $(function(){
-  $('.top__wrapper__backs__back1').slideUp(1800);
+  $('.top__wrapper__backs__back1').slideUp(1500);
   let openSite = function(){
-    $('.top__wrapper__backs__back2').slideUp(1500);
+    $('.top__wrapper__backs__back2').slideUp(1200);
   }
-  setTimeout(openSite, 10000)
+  setTimeout(openSite, 9000)
 });
 
 
