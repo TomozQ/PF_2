070cec7d0a5a2f51f577525c81c12ef551f82ae0
TomozQ <inf.4clover@gmail.com>
2020-09-16 15:57:45 +0900

    コメント投稿画面でも非同期が働いたので修正

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -57,7 +57,7 @@ $(function(){
       return html;
     };
   }
-  $('Form').on('submit', function(e){
+  $('.Form').on('submit', function(e){
     e.preventDefault()
     let formData = new FormData(this);
     let url = $(this).attr('action');
